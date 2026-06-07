79040f48a4fb37813bc8eed5941e4fd11077e1ac
denkavia <denkavia.contactme@proton.me>
2026-06-08 00:33:29 +0700

    feat: add basic navigation between block

--- src/components/tp-editor/tp-editor.tsx
+++ src/components/tp-editor/tp-editor.tsx
@@ -4,7 +4,7 @@ import {
   renderTpBlock,
   TpBlockType,
 } from "@/components/tp-editor/tp-block-type";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const initialDocumentBlockTree: TpBlockType[] = [
   {
@@ -30,6 +30,10 @@ export default function TpEditor() {
     "initial-block-2",
   );
 
+  const cursorPlacement = useRef<"start" | "end">("start");
+
+  const pressedKey = useRef<string[]>([]);
+
   const [documentBlockTree, setDocumentBlockTree] = React.useState<
     TpBlockType[]
   >(initialDocumentBlockTree);
@@ -48,11 +52,71 @@ export default function TpEditor() {
     });
   };
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (!pressedKey.current.includes(e.key)) {
+      pressedKey.current.push(e.key);
+    }
+
+    if (pressedKey.current.length == 1) {
+      if (pressedKey.current[0] === "ArrowLeft") {
+        const cursorData = window.getSelection();
+        if (cursorData && (cursorData?.focusOffset ?? 0) == 0) {
+          e.preventDefault();
+          const blockIndex = documentBlockTree.findIndex(
+            (block) => block.id === focusedBlockId,
+          );
+
+          if (blockIndex > 0) {
+            cursorPlacement.current = "end";
+            setFocusedBlockId(documentBlockTree[blockIndex - 1].id);
+          }
+        }
+      }
+      if (pressedKey.current[0] === "ArrowRight") {
+        const cursorData = window.getSelection();
+
+        if (cursorData && cursorData.focusNode?.nodeValue?.length) {
+          if (cursorData.focusOffset >= cursorData.focusNode.nodeValue.length) {
+            const blockIndex = documentBlockTree.findIndex(
+              (block) => block.id === focusedBlockId,
+            );
+
+            if (blockIndex < documentBlockTree.length - 1) {
+              cursorPlacement.current = "start";
+              setFocusedBlockId(documentBlockTree[blockIndex + 1].id);
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const onKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (pressedKey.current.includes(e.key)) {
+      pressedKey.current = pressedKey.current.filter((key) => e.key !== key);
+    }
+  };
+
   useEffect(() => {
     if (focusedBlockId) {
-      console.log("Focused block id: " + focusedBlockId);
-      console.log(document.getElementById(focusedBlockId));
-      document.getElementById(focusedBlockId)?.focus();
+      const focusedElement = document.getElementById(focusedBlockId);
+
+      if (!focusedElement) return;
+
+      focusedElement.focus();
+      const textNode = focusedElement.firstChild;
+      const cursorData = window.getSelection();
+
+      if (textNode && cursorData) {
+        console.log(textNode);
+        const length = textNode.textContent?.length ?? 0;
+        const offset = cursorPlacement.current === "end" ? length : 0;
+        const range = document.createRange();
+        range.setStart(textNode, offset);
+        range.collapse(true);
+        cursorData.removeAllRanges();
+        cursorData.addRange(range);
+      }
     }
   }, [focusedBlockId]);
 
@@ -70,7 +134,7 @@ export default function TpEditor() {
 
   return (
     <div>
-      <div className={`border`}>
+      <div className={`border`} onKeyDown={onKeyDown} onKeyUp={onKeyUp}>
         {documentBlockTree.map((block: TpBlockType) => {
           return renderTpBlock(block, blockChangeHandler, onFocus, onBlur);
         })}
