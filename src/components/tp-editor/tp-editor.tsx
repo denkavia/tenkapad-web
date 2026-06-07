1c57b8fab50f75a0a05bf66a5398b86f72988d9b
denkavia <denkavia.contactme@proton.me>
2026-06-07 21:06:14 +0700

    chore: remove all editor functionality (i need to rebuild it again)

--- src/components/tp-editor/tp-editor.tsx
+++ src/components/tp-editor/tp-editor.tsx
@@ -1,72 +1,9 @@
-"use client";
-
-import TpBlock, { BlockProps } from "@/components/tp-editor/tp-block";
-import React, { useEffect, useRef, useState } from "react";
+"use client"
 
 export default function TpEditor() {
-  const [blocks, setBlocks] = useState<BlockProps[]>([
-    { uid: "initial-id", content: "Write here...", type: "text" },
-  ]);
-  const [activeBlockId, setActiveBlockId] = useState<string | null>(null);
-  const pressedKeys = useRef<Set<string>>(new Set());
-
-  const addNewBlock = () => {
-    const uid = crypto.randomUUID();
-
-    setBlocks((prev) => [
-      ...prev,
-      {
-        uid,
-        content: "",
-        type: "text",
-      },
-    ]);
-
-    setActiveBlockId(uid);
-  };
-
-  const keydownHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    pressedKeys.current.add(e.key);
-
-    if (pressedKeys.current.has("Enter")) {
-      if (!pressedKeys.current.has("Shift")) {
-        e.preventDefault();
-        addNewBlock();
-      }
-    }
-  };
-
-  const keyupHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
-    pressedKeys.current.delete(e.key);
-  };
-
-  useEffect(() => {
-    if (activeBlockId) {
-      const targetElement = document.getElementById(activeBlockId);
-
-      if (targetElement) {
-        targetElement.focus();
-      }
-    }
-  }, [activeBlockId, blocks]);
-
   return (
-    <div
-      className={`w-full border-2 p-4`}
-      onKeyDown={keydownHandler}
-      onKeyUp={keyupHandler}
-    >
-      <div id={`block-area`}>
-        {blocks.map((block) => (
-          <TpBlock
-            key={block.uid}
-            uid={block.uid}
-            content={block.content}
-            type={block.type}
-            childs={block.childs ?? []}
-          />
-        ))}
-      </div>
+    <div className={`border`}>
+      Testing
     </div>
-  );
+  )
 }
